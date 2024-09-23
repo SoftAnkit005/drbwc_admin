0c1d3f5096245d9ce257d68164ccb49f3fe17b5b
nitesh-softions <nitesh.softieons@gmail.com>
2024-09-23 14:26:39 +0530

    - profile name error solved

--- src/layouts/header/ProfileDD.js
+++ src/layouts/header/ProfileDD.js
@@ -9,8 +9,8 @@ const ProfileDD = () => {
       <div className="d-flex gap-3 p-3 border-bottom pt-2 align-items-center">
         <img src={user1} alt="user" className="rounded-circle" width="55" />
         <span>
-          <h5 className="mb-0 fw-medium">{user.full_name}</h5>
-          <small className='text-muted'>{user.email}</small>
+          <h5 className="mb-0 fw-medium">{user?.full_name}</h5>
+          <small className='text-muted'>{user?.email}</small>
         </span>
       </div>
     </div>
