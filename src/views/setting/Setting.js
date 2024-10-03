06db2ca8d9aba871c08d8118fdb7750160b24d6e
nitesh-softions <nitesh.softieons@gmail.com>
2024-10-03 17:41:01 +0530

    - Unwanted tabs in setting section is commented

--- src/views/setting/Setting.js
+++ src/views/setting/Setting.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import { Card, CardBody, Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
 import './settings.scss'
-import StoreSetting from './settingtabs/StoreSetting';
-import PaymentSetting from './settingtabs/PaymentSetting';
-import ShippingSetting from './settingtabs/ShippingSetting';
+// import StoreSetting from './settingtabs/StoreSetting';
+// import PaymentSetting from './settingtabs/PaymentSetting';
+// import ShippingSetting from './settingtabs/ShippingSetting';
 import TaxSetting from './settingtabs/TaxSettings';
 
 const Setting = () => {
-  const [activeTab, setActiveTab] = useState('1'); // Step 1: State for active tab
+  const [activeTab, setActiveTab] = useState('4'); // Step 1: State for active tab
 
   const toggle = (tab) => {
     if (activeTab !== tab) {
@@ -19,7 +19,7 @@ const Setting = () => {
     <Card>
       <CardBody>
         <Nav tabs>
-          <NavItem>
+          {/* <NavItem>
             <NavLink className={activeTab === '1' ? 'active' : ''} onClick={() => toggle('1')} > Store Setting </NavLink>
           </NavItem>
           <NavItem>
@@ -27,13 +27,13 @@ const Setting = () => {
           </NavItem>
           <NavItem>
             <NavLink className={activeTab === '3' ? 'active' : ''} onClick={() => toggle('3')} > Shipping Setting </NavLink>
-          </NavItem>
+          </NavItem> */}
           <NavItem>
             <NavLink className={activeTab === '4' ? 'active' : ''} onClick={() => toggle('4')} > Tax Setting </NavLink>
           </NavItem>
         </Nav>
         <TabContent activeTab={activeTab}>
-          <TabPane tabId="1">
+          {/* <TabPane tabId="1">
             <StoreSetting />
           </TabPane>
           <TabPane tabId="2">
@@ -41,7 +41,7 @@ const Setting = () => {
           </TabPane>
           <TabPane tabId="3">
             <ShippingSetting />
-          </TabPane>
+          </TabPane> */}
           <TabPane tabId="4">
             <TaxSetting pageName="tax" />
           </TabPane>
